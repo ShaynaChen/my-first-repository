748cc6c400dbf1f8a8719e590f8982e1dd5f35b5
shayna <111111@qq.com>
2023-02-03 20:32:29 +0800

    3

---
+++ run.py
@@ -0,0 +1,25 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from python_class.common import search
+from test_data import test_data1
+
+options = Options()
+options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=options)
+url = test_data1.url["url"]
+user = test_data1.user["username"]
+pwd = test_data1.user["password"]
+s_key = test_data1.s_key["s_key"]
+
+result = search(driver, url, user, pwd, s_key)
+
+if s_key in result:
+    print("案例通过")
+else:
+    print("案例不通过")
+
+time.sleep(1)
+driver.quit()
